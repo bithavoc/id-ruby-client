3a0a27c1f25e56ebc965334c33bc1eaef67e3f48
thepumpkin1979 <johan@heapsource.com>
2014-09-22 17:46:20 -0500

    load operations by file scan

--- lib/bithavoc/identity/client.rb
+++ lib/bithavoc/identity/client.rb
@@ -1,13 +1,8 @@
 require "bithavoc/identity/version"
 require "bithavoc/identity/error"
 
-# operations
 require "bithavoc/identity/connectivity"
-require "bithavoc/identity/negotiate"
-require "bithavoc/identity/signin"
-require "bithavoc/identity/signup"
-require "bithavoc/identity/confirm"
-require "bithavoc/identity/validate"
+Dir.glob( File.join( File.dirname(__FILE__), 'operations', '*.rb' ), &method(:require) )
 
 module Bithavoc
     module Identity
@@ -20,13 +15,12 @@ module Bithavoc
             include Confirm
             include Validate
 
-            public
             attr_reader :app_id
 
             def initialize(app_id) 
                 @app_id = app_id
             end
 
-        end # Client
-    end # Identity
-end # Bithavoc
+        end
+    end
+end
